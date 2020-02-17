d1216b5689360a23d1316231e6f63efaeb531d90
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-17 21:58:10 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+});
